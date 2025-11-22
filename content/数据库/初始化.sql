afd2f1d3a518138a47fd73a4f43c7c8a458053b7
404NWF3 <sven30hardner@gmail.com>
2025-11-22 12:02:43 +0800

    Update DataBase Notes

---
+++ content/数据库/初始化.sql
@@ -0,0 +1,148 @@
+drop table Order_items;
+drop table Product;
+drop table Ptype;
+drop table Orders;
+drop table Payment;
+drop table Customer;
+
+CREATE TABLE Customer (
+    CNO CHAR(5) PRIMARY KEY, 
+    CNAME NVARCHAR2(50), 
+    COMPANY NVARCHAR2(100), 
+    CITY NVARCHAR2(50), 
+    TEL VARCHAR(20)
+);
+
+CREATE TABLE Ptype (
+    TNO CHAR(1) PRIMARY KEY,
+    TNAME NVARCHAR2(15)
+);
+
+CREATE TABLE Product (
+    PNO char(4) PRIMARY KEY,
+    PNAME NVARCHAR2(100),
+    PRICE NUMBER(7, 2),
+    TNO CHAR(1),
+    INVENTORY INT,
+    FOREIGN KEY (TNO) REFERENCES Ptype(TNO)
+);
+
+CREATE TABLE Payment (
+    PAYMENT_TNO char(1) PRIMARY KEY,
+    PAYMENT_TYPE NVARCHAR2(50)
+);
+
+CREATE TABLE Orders (
+    ONO CHAR(5) PRIMARY KEY,
+    ORDER_DATE DATE,
+    CNO CHAR(5),
+    FREIGHT INT,
+    SHIPMENT_DATE DATE,
+    CITY NVARCHAR2(50),
+    PAYMENT_TNO char(1),
+    STATUS NVARCHAR2(20),
+    FOREIGN KEY (CNO) REFERENCES Customer(CNO),
+    FOREIGN KEY (PAYMENT_TNO) REFERENCES Payment(PAYMENT_TNO)
+);
+
+CREATE TABLE Order_items (
+    ONO CHAR(5),
+    PNO char(4),
+    QTY INT,
+    DISCOUNT DECIMAL(4, 2),
+    PRIMARY KEY (ONO, PNO),
+    FOREIGN KEY (ONO) REFERENCES Orders(ONO), 
+    FOREIGN KEY (PNO) REFERENCES Product(PNO)
+);
+
+INSERT INTO CUSTOMER 
+VALUES('C0001','Zhang Chen','Citibank','Shanghai','021-65903818');
+INSERT INTO CUSTOMER 
+VALUES('C0002','Wang Ling','Oracle','Beijing','010-62754108');
+INSERT INTO CUSTOMER 
+VALUES('C0003','Li Li','Minsheng bank','Shanghai','021-62438210');
+INSERT INTO CUSTOMER 
+VALUES('C0004','Liu Xin','Citibank','Shanghai','021-55392225');
+INSERT INTO CUSTOMER 
+VALUES('C0005','Xu Ping','Microsoft','Beijing','010-43712345');
+INSERT INTO CUSTOMER 
+VALUES('C0006','Zhang Qing','Freightliner LLC','Guangzhou','020-84713425');
+INSERT INTO CUSTOMER 
+VALUES('C0007','Yang Jie','Freightliner LLC','Guangzhou','020-76543657');
+INSERT INTO CUSTOMER 
+VALUES('C0008','Wang Peng','IBM','Beijing','010-62751231');
+INSERT INTO CUSTOMER 
+VALUES('C0009','Du Wei','HoneyWell','Shanghai','021-45326788');
+INSERT INTO CUSTOMER 
+VALUES('C0010','Shan Feng','Oracle','Beijing','010-62751230');
+
+COMMIT;
+
+-- 后面不要运行
+
+INSERT INTO Product VALUES('1001','Advanced Marketing',20.50,'1',120);
+INSERT INTO Product VALUES('1002','Visual Basic Programming',28.00,'1',200);
+INSERT INTO Product VALUES('1003','Computer Application',30.55,'1', 80);
+INSERT INTO Product VALUES('1004','An Introduction to Database Systems',20.00,'1',12);
+INSERT INTO Product VALUES('1005','Microecononics',35.80,'1',150);
+INSERT INTO Product VALUES('2001','The Lion King',35.00	,'2',150);
+INSERT INTO Product VALUES('2002','Classic Disney',25.00	,'2',20);
+INSERT INTO Product VALUES('3001','Microsoft Money 2010',70.50,'3',300);
+INSERT INTO Product VALUES('3002','Microsoft Student 2009 ',80.00,'3',150);
+INSERT INTO Product VALUES('3003','Norton AntiVirus 2014', 40.90,'3',250);
+INSERT INTO Product VALUES('3004','Math Advantage 2007', 30.00,'3',10);
+
+INSERT INTO Orders 
+VALUES('O0001',TO_DATE('10-03-2014','DD-MM-YYYY'),'C0001',8,TO_DATE('11-03-2014','DD-MM-YYYY'),'Beijing','1','Complete');
+INSERT INTO Orders 
+VALUES('O0002',TO_DATE('11-03-2014','DD-MM-YYYY'),'C0002',8,TO_DATE('12-03-2014','DD-MM-YYYY'),'Shanghai','2','Complete');
+INSERT INTO Orders 
+VALUES('O0003',TO_DATE('11-03-2014','DD-MM-YYYY'),'C0009',5,TO_DATE('12-03-2014','DD-MM-YYYY'),'Shanghai','2','Complete');
+INSERT INTO Orders 
+VALUES('O0004',TO_DATE('13-04-2014','DD-MM-YYYY'),'C0007',5,TO_DATE('15-04-2014','DD-MM-YYYY'),'Beijing','1','Complete');
+INSERT INTO Orders 
+VALUES('O0005',TO_DATE('14-04-2014','DD-MM-YYYY'),'C0010',8,TO_DATE('16-04-2014','DD-MM-YYYY'),'Beijing','1','Complete');
+INSERT INTO Orders 
+VALUES('O0006',TO_DATE('25-04-2014','DD-MM-YYYY'),'C0008',5,TO_DATE('26-04-2014','DD-MM-YYYY'),'Shanghai','3','Complete');
+INSERT INTO Orders 
+VALUES('O0007',TO_DATE('26-05-2014','DD-MM-YYYY'),'C0010',8,TO_DATE('28-05-2014','DD-MM-YYYY'),'Shanghai','3','Complete');
+INSERT INTO Orders 
+VALUES('O0008',TO_DATE('17-06-2014','DD-MM-YYYY'),'C0006',5,TO_DATE('18-06-2014','DD-MM-YYYY'),'Beijing','1','Complete');
+INSERT INTO Orders 
+VALUES('O0009',TO_DATE('21-07-2014','DD-MM-YYYY'),'C0008',5,TO_DATE('22-07-2014','DD-MM-YYYY'),'Shanghai','2','in process');
+INSERT INTO Orders 
+VALUES('O0010',TO_DATE('23-07-2014','DD-MM-YYYY'),'C0005',5,TO_DATE('25-07-2014','DD-MM-YYYY'),'Beijing','1','in process');
+
+INSERT INTO Order_items(ONO,PNO,QTY) VALUES('O0001','1001',5);
+INSERT INTO Order_items VALUES('O0001','1002',1,0.2);
+INSERT INTO Order_items VALUES('O0001','1003',3,0.3);
+INSERT INTO Order_items VALUES('O0001','2001',1,0.2);
+INSERT INTO Order_items VALUES('O0001','2002',1,0.2);
+INSERT INTO Order_items VALUES('O0002','1001',2,0);
+INSERT INTO Order_items VALUES('O0002','1004',5,0.4);
+INSERT INTO Order_items VALUES('O0002','1005',1,0.05);
+INSERT INTO Order_items VALUES('O0002','3003',3,0.3);
+INSERT INTO Order_items VALUES('O0006','1004',5,0.4);
+INSERT INTO Order_items(ONO,PNO,QTY) VALUES('O0006','1005',1);
+INSERT INTO Order_items VALUES('O0006','2001',2,0.3);
+INSERT INTO Order_items VALUES('O0006','2002',1,0.2);
+INSERT INTO Order_items VALUES('O0006','3003',2,0.3);
+INSERT INTO Order_items VALUES('O0007','1004',1,0.4);
+INSERT INTO Order_items VALUES('O0007','3004',3,0.2);
+INSERT INTO Order_items VALUES('O0008','1002',1,0.2);
+INSERT INTO Order_items VALUES('O0008','2001',3,0.3);
+INSERT INTO Order_items VALUES('O0009','3003',2,0.3);
+INSERT INTO Order_items(ONO,PNO,QTY) VALUES('O0009','1001',1);
+INSERT INTO Order_items VALUES('O0010','1005',1,0.05);
+INSERT INTO Order_items VALUES('O0003','1005',1,0.05);
+INSERT INTO Order_items VALUES('O0004','1005',1,0.05);
+INSERT INTO Order_items VALUES('O0005','1005',1,0.05);
+
+INSERT INTO Payment VALUES('1','Cash');
+INSERT INTO Payment VALUES('2','Check');
+INSERT INTO Payment VALUES('3','Credit card');
+INSERT INTO Payment VALUES('4','Telegraphic money');
+
+INSERT INTO Ptype VALUES(1,'Book');
+INSERT INTO Ptype VALUES(2,'CD');
+INSERT INTO Ptype VALUES(3,'Software');
